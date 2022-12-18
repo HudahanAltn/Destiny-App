5c1e2d43eb662a23f29d4d295cf56e8683d9792d
HudahanAltn <dev.hudahanaltun@gmail.com>
2022-12-18 23:22:37 +0300

    Initial Commit

---
+++ Destini App/Controller/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Destini App
+//
+//  Created by Hüdahan Altun on 11.10.2022.
+//
+
+import UIKit
+
+
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
+}
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    
+    @IBOutlet weak var choice1Button: UIButton!
+    
+    
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+        
+        choice1Button.tintColor = UIColor(rgb:  0xDBC8AC)
+        choice2Button.tintColor = UIColor(rgb:  0xDBC8AC)
+
+    }
+
+
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+    }
+    
+   
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+    
+}
+
---
+++ Destini App/Modal/Story.swift
@@ -0,0 +1,19 @@
+//
+//  Story.swift
+//  Destini App
+//
+//  Created by Hüdahan Altun on 11.10.2022.
+//
+
+import Foundation
+
+
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
+
